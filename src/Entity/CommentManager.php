d29c8ae0217a8eb11e21c9e40deaf621920fecd5
Oskar Stark <oskarstark@googlemail.com>
2020-02-12 09:01:07 +0100

    CS fix

--- src/Entity/CommentManager.php
+++ src/Entity/CommentManager.php
@@ -131,7 +131,8 @@ class CommentManager extends BaseEntityManager implements CommentManagerInterfac
     {
         switch ($this->getConnection()->getDriver()->getDatabasePlatform()->getName()) {
             case 'postgresql':
-                return sprintf(<<<'SQL'
+                return sprintf(
+                    <<<SQL
 UPDATE %s SET comments_count = count_comment.total
 FROM (SELECT c.post_id, count(*) AS total FROM %s AS c WHERE c.status = 1 GROUP BY c.post_id) count_comment
 WHERE %s.id = count_comment.post_id
@@ -142,7 +143,8 @@ SQL,
                 );
 
             default:
-                return sprintf(<<<'SQL'
+                return sprintf(
+                    <<<SQL
 UPDATE %s p, (SELECT c.post_id, count(*) as total FROM %s as c WHERE c.status = 1 GROUP BY c.post_id) as count_comment
 SET p.comments_count = count_comment.total
 WHERE p.id = count_comment.post_id
